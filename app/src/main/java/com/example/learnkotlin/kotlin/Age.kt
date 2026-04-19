e90d8750faed0d611e07204def7bb97604a91f8c
Nguyen Trung Kien <nguyentrungkien19101999@gmail.com>
2026-04-19 09:14:04 +0700

    first commit

---
+++ app/src/main/java/com/example/learnkotlin/kotlin/Age.kt
@@ -0,0 +1,40 @@
+package com.example.learnkotlin.kotlin
+
+import android.renderscript.Int2
+
+fun information(age: Int) {
+//    if (age == 20) {
+//        println("Da du tuoi lay vo")
+//    } else if (age > 20) {
+//        println("Lay vo nhanh khong e")
+//    } else {
+//        println("Chua du tuoi lay vo")
+//    }
+    when(age) {
+        in 1..19 -> {
+            println("$age tuoi van con non va xanh lam!")
+        }
+
+        in 20..27 -> {
+            println("$age tuoi roi lay vo di khong e bay gio!")
+        }
+
+        else -> {
+            println("$age tuoi thi e xu no roi!")
+        }
+    }
+}
+
+fun getAge(age1: Int, age2: Int) : Int {
+    return age1 - age2
+}
+
+internal fun getRole(name: String) {
+    if (name == "Nhat Minh") {
+        println("$name la con")
+    } else if (name == "Trung Kien") {
+        println("$name la bo")
+    } else {
+        println("$name la me")
+    }
+}
---
+++ app/src/main/java/com/example/learnkotlin/kotlin/Main.kt
@@ -0,0 +1,14 @@
+package com.example.learnkotlin.kotlin
+
+fun main() {
+    var ageFather: Int = 28
+    var ageSon: Int = 1
+
+    information(ageFather)
+
+    var age: Int = getAge(ageFather, ageSon)
+
+    println("Con trai it hon bo $age tuoi")
+
+    getRole("Trung Kien")
+}
